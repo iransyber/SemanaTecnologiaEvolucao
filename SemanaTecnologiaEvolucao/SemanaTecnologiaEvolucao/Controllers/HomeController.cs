0870b8d1037f1d9d45c2d4fd2a5561ce5aac3e0f
iran junior <iran_syber@hotmail.com>
2016-04-30 01:25:05 -0300

    Aplicando tema ao sistema

--- SemanaTecnologiaEvolucao/SemanaTecnologiaEvolucao/Controllers/HomeController.cs
+++ SemanaTecnologiaEvolucao/SemanaTecnologiaEvolucao/Controllers/HomeController.cs
@@ -8,10 +8,8 @@ namespace SemanaTecnologiaEvolucao.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Login()
         {
-            ViewBag.Title = "Home Page";
-
             return View();
         }
     }
