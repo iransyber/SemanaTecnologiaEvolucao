288b4e72ec0d35fffa581f7608173bf54a1fcd3b
iran junior <iran_syber@hotmail.com>
2016-05-04 21:06:11 -0300

    Inclusao do angular e funcionando tela de login

---
+++ SemanaTecnologiaEvolucao/SemanaTecnologiaEvolucao/Controllers/dashboardController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SemanaTecnologiaEvolucao.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        // GET: dashboard
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
